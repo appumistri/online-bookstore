6670d8cf4930591ba96ed62579b2436ae268636c
Appu Mistri <appu.mistri@cesltd.com>
2020-06-04 13:33:03 +0530

    Added list of categories from database to applicaton.

--- angular-bookstore/src/app/services/book.service.ts
+++ angular-bookstore/src/app/services/book.service.ts
@@ -3,26 +3,40 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Book } from '../common/book';
+import { BookCategory } from '../common/book-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  private baseUrl: string = "http://localhost:8080/api/v1/books";
+  private booksUrl: string = "http://localhost:8080/api/v1/books";
+  private bookCategoryUrl: string = "http://localhost:8080/api/v1/book-category";
 
   constructor(private httpClient: HttpClient) { }
 
   getBooks(categoryId: number): Observable<Book[]> {
-    const searchUrl = `${this.baseUrl}/search/categoryid?id=${categoryId}`;
+    const searchUrl = `${this.booksUrl}/search/categoryid?id=${categoryId}`;
     return this.httpClient.get<GetBookResponse>(searchUrl).pipe(
       map(response => response._embedded.books)
     );
+  }
+
+  getBookCategory(): Observable<BookCategory[]> {
+    return this.httpClient.get<GetBookCategoryresponse>(this.bookCategoryUrl).pipe(
+      map(response => response._embedded.bookCategory)
+    )
   }
 }
 
 interface GetBookResponse {
   _embedded: {
     books: Book[];
+  }
+}
+
+interface GetBookCategoryresponse {
+  _embedded: {
+    bookCategory: BookCategory[];
   }
 }
--- bookstore/src/main/java/onlinebookstore/bookstore/config/RepositoryConfig.java
+++ bookstore/src/main/java/onlinebookstore/bookstore/config/RepositoryConfig.java
@@ -18,5 +18,6 @@ public class RepositoryConfig implements RepositoryRestConfigurer {
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 		config.exposeIdsFor(
 				entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
+		config.getCorsRegistry().addMapping("/**").allowedOrigins("*");
 	}
 }
